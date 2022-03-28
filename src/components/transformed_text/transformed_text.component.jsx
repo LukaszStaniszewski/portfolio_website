22f4ff8cde7a78d9d2ab2c0e09fb71643eebfede
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-03-28 23:14:43 +0200

    home page ver.2

--- src/components/transformed_text/transformed_text.component.jsx
+++ src/components/transformed_text/transformed_text.component.jsx
@@ -2,44 +2,20 @@ import { Fragment, useEffect } from "react"
 
 import "./transformed_text.styles.scss"
 
-// const TransformedText = ({text, color, ...otherProps}) => {
-//     // useEffect(() => {
-//     //     document.querySelector(".lala1").innerHTML = text
-//     // }, [])
-//    const splitText = text.split(" ");
-//     console.log(splitText)
-//     return (
-//     // <div className="lala1" {...otherProps} ></div>
-//     <Fragment>
-//     {
-//         splitText.map((word, index) => {
-//           return  <span key={index} className={color[word] ? `${color[word]}`: 'text-quaternary'}>{word}</span>
-//         //   return  <div key={index} style={{color: `var(--color-text-primary)`}}>{word}</div>
-//         })
-//     }
-  
-//     </Fragment>
-//     )
-// }
-
-// export default TransformedText
-
 const TransformedText = ({text, color, ...otherProps}) => {
-  // useEffect(() => {
-  //     document.querySelector(".lala1").innerHTML = text
-  // }, [])
+
  const splitText = text.split(" ");
   console.log(splitText)
   return (
-  // <div className="lala1" {...otherProps} ></div>
+  
   <Fragment>
-  {
+    {
       splitText.map((word, index) => {
-        return  <span key={index} className={color[word] ? `${color[word]}`: 'text-quaternary'}>{word}</span>
-      //   return  <div key={index} style={{color: `var(--color-text-primary)`}}>{word}</div>
+        if(word === "<br/>")  return  <div key={index} className={color[word] ? `${color[word]}`: 'text-quaternary'} {...otherProps}>{word === "<br/>" ? " " : word}</div>
+        
+        return  <span key={index} className={color[word] ? `${color[word]}`: 'text-quaternary'} {...otherProps}>{word}</span>
       })
-  }
-
+    }
   </Fragment>
   )
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import TransformedText from "../../components/transformed_text/transformed_text.component"
 import "./home.styles.scss"
 
@@ -10,36 +9,20 @@ const TEXT_COLORS = {
     '()': 'bracket-primary',
     '=>': 'text-primary',
     '{': 'bracket-primary',
-
+    '}': 'bracket-primary',
+    '(': 'bracket-secondary',
+    ')': 'bracket-secondary',
+    '"': 'color-quotation-marks',
+    'console.': 'text-secondary',
+    'log': 'text-tertiary'
 }
 
 const Home = () => {
-    useEffect(()=> {
-        document.getElementById("arrow").innerHTML = "=>"
-        document.getElementById("right-bracket").innerHTML = "{"
-        document.getElementById("left-bracket").innerHTML = "}"
-    },[])
+  
     const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-    // const part1 = 'const Hi = () => {'
-    // const part2 = 'console.log('
-    // const part3 = '"I am Łukasz'
-    // const part4 = 'Front End Developer"'
-    // const part5 = ')'
-    // const part6 = '}'
-    const part = {
-        part1: 'const Hi = () => {',
-        part2: 'console.log(',
-        part3: '"I am Łukasz',
-        part4: 'Front End Developer"',
-        part5: ')',
-        part6: '}',
-    }
-   
-    // const welcomeText = 'const Hi = () => { <br/>&nbsp console.log(<br/>&nbsp&nbsp "I am Łukasz<br/>&nbsp&nbsp Front End Developer<br/>&nbsp )<br/>})'
-    const welcomeText = 'const Hi = () => { console.log ( "I am Łukasz Front End Developer" ) } )'
 
-
-
+    const welcomeText = 'const Hi = () => { <br/> console. log ( <br/> " I am Łukasz <br/> Front End Developer " <br/> ) <br/> }'
+  
     return (
         <section className="home-container" >
             <div className="home-rows">{
@@ -49,35 +32,7 @@ const Home = () => {
             }</div>
             <div >
                 <div className="home-welcome-text" >
-                    {/* <TransformedText text={welcomeText} color={TEXT_COLORS}/> */}
-                   
-                        <span className="text-primary">const</span>
-                        <span className="text-tertiary">Hi</span>
-                        <span className="text-primary">=</span>
-                        <span className="bracket-primary">()</span>
-                        <span className="text-primary" id="arrow"></span>
-                        <span className="bracket-primary" id="right-bracket"></span> 
-                        <br />
-                   
-                        <span className="text-secondary">console.</span>
-                        <span className="text-tertiary">log</span>
-                        <span className="bracket-secondary">(</span>
-                        <br />
-                    
-                        <span className="color-quotation-marks">"</span>
-                        <span className="text-quaternary">I'm Łukasz</span>
-                        <br />
-                    
-                        <span className="text-quaternary">Front End Developer</span>
-                        <span className="color-quotation-marks">"</span>
-                        <br />
-                 
-                        <span className="bracket-secondary" >)</span>
-                   
-                    <div>
-                        <span className="bracket-primary" id="left-bracket"></span>
-                    </div>
-                    
+                    <TransformedText text={welcomeText} color={TEXT_COLORS}/> 
                 </div>
             </div>
         </section>
