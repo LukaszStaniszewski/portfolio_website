89cee86054e30f74b1c17d9090410a0c20032e68
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-04-10 22:21:25 +0200

    removed unnecessary comments

--- src/components/custom_link/custom_link.component.jsx
+++ src/components/custom_link/custom_link.component.jsx
@@ -5,33 +5,6 @@ import React from "react"
 const LINK_TYPE_CLASS = {
   header: 'header-link-clicked'
 }
-const DESTINATION = {
-    home: '/',
-    readme: 'readme' ,
-    projects: 'projects',
-    contact: 'contact',
-}
-
-// const TEST = {
-//     header: {
-//         home: '/' && 'header-link-clicked',
-//         readme: 'readme' && 'header-link-clicked',
-//         projects: 'projects' && 'header-link-clicked',
-//         contact: 'contact' && 'header-link-clicked',
-//     }
-// }
-
-// const CustomLink = ({children, classType, active, to, ...otherProps}) => {
-//     console.log("active",active)
-//     console.log(to, LINK_TYPE_CLASS[classType])
-//     return<Link 
-//         className={`default-link ${active === to && LINK_TYPE_CLASS[classType]}`}
-//         to={to} {...otherProps}
-//         >{children}
-//     </Link>
-// }
-
-
 const CustomLink = ({children, classType, active,  ...otherProps}) => {
 
    
@@ -41,15 +14,5 @@ const CustomLink = ({children, classType, active,  ...otherProps}) => {
         >{children}
     </Link>
 }
-
-// const CustomLink = ({children, classType, active,  ...otherProps}) => {
-//     console.log("active",active)
-//     console.log(LINK_TYPE_CLASS[classType])
-//     return<Link 
-//         className={`default-link ${active === otherProps.to && DESTINATION[classType]}`}
-//          {...otherProps}
-//         >{children}
-//     </Link>
-// }
 
 export default React.memo(CustomLink);
