09772ccbf849df21c92a3d7b2d5647b45217107e
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-08-26 13:54:41 +0200

    projects page mobile divices resolution fix

---
+++ src/components/project-frame/project-frame.component.jsx
@@ -0,0 +1,37 @@
+import { ReactComponent as GitLogo } from "../../assets/tech-skills/git-logo.svg";
+import { ReactComponent as GitLogoLight } from "../../assets/tech-skills/git-logo-light.svg";
+import { useThemeContext } from "../../context/theme/theme.context";
+import CustomButton from "../custom_button/custom_button.component";
+import "./project-frame.styles.scss";
+
+const ProjectFrame = ({ data }) => {
+   const { currentTheme } = useThemeContext();
+   const { title, text, image, technologies, url, github } = data;
+
+   return (
+      <div className="project-container">
+         <div className="project-content">
+            <h3 className="project-content--title">{title}</h3>
+            <span className="project-content--about">{text}</span>
+            <ul className="project-content--technologies">
+               {technologies.map((tech, index) => (
+                  <li key={index}>{tech}</li>
+               ))}
+            </ul>
+            <div className="project-content__button-container">
+               <CustomButton newTab={url}>Visit{`\xa0`}site</CustomButton>
+               <a href={github} target="_blank" className="github-link">
+                  {currentTheme ? (
+                     <GitLogoLight className="git-logo" />
+                  ) : (
+                     <GitLogo className="git-logo" />
+                  )}
+               </a>
+            </div>
+         </div>
+         <div className="project-image" style={{ backgroundImage: `url(${image})` }} />
+      </div>
+   );
+};
+
+export default ProjectFrame;
--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -1,34 +1,28 @@
-import {ReactComponent as ScrollMouse} from "../../assets/mouse.svg"
-import {ReactComponent as ScrollMouseLight} from "../../assets/mouse-light.svg";
+import { ReactComponent as ScrollMouse } from "../../assets/mouse.svg";
+import { ReactComponent as ScrollMouseLight } from "../../assets/mouse-light.svg";
 
-import Project from "../../components/project/project.component"
-import PROJECTS_DATA from "./projects_data"
-import {useThemeContext} from "../../context/theme/theme.context"
+import ProjectFrame from "../../components/project-frame/project-frame.component";
+import PROJECTS_DATA from "./projects_data";
+import { useThemeContext } from "../../context/theme/theme.context";
 
-import "./projects.styles.scss"
+import "./projects.styles.scss";
 
 const Projects = () => {
-const {currentTheme} = useThemeContext()
-const executeScroll = () =>  window.scrollTo(0, (window.innerHeight))
+   const { currentTheme } = useThemeContext();
+   const executeScroll = () => window.scrollTo(0, window.innerHeight - 50);
 
-    return (
-        <section className="projects-container">
-            <button className="scroll-button" onClick={executeScroll}>
-                {currentTheme
-                    ?<ScrollMouseLight/>
-                    :<ScrollMouse   />}
-             </button>
-            {
-                PROJECTS_DATA.map((project, index) => {
-                    if(index === 1) return <Project key={index} data={project}/>
-                    
-    
-                    return <Project key={index} data={project} ></Project>
-                })
-            }
-        
-        </section>
-    )
-}
+   return (
+      <section className="projects-container">
+         <button className="scroll-button" onClick={executeScroll}>
+            {currentTheme ? <ScrollMouseLight /> : <ScrollMouse />}
+         </button>
+         {PROJECTS_DATA.map((project, index) => {
+            if (index === 1) return <ProjectFrame key={index} data={project} />;
 
-export default Projects
+            return <ProjectFrame key={index} data={project}></ProjectFrame>;
+         })}
+      </section>
+   );
+};
+
+export default Projects;
