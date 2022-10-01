5bd75d23851ce3fb3ee812a80ff01d3d5400b3ba
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-10-01 23:03:04 +0200

    change loading animation

--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -5,39 +5,38 @@ import { Fragment } from "react";
 import ProjectCard from "../../components/project-card/project-card.component";
 import PROJECTS_DATA from "./projects_data";
 import { useThemeContext } from "../../context/theme/theme.context";
-import Spinner from "../../components/spinner/spinner.component";
 
 import "./projects.styles.scss";
 
 const Projects = () => {
    const { currentTheme } = useThemeContext();
    const executeScroll = () => window.scrollTo(0, window.innerHeight - 50);
-   return <Spinner></Spinner>;
-   // return (
-   //    <section className="projects-container">
-   //       {PROJECTS_DATA.map((project, index) => {
-   //          if (index === 0)
-   //             return (
-   //                <div className="first-project">
-   //                   <div style={{ marginTop: "10vh" }} />
-   //                   <ProjectCard shakeAnimation={true} key={index} data={project} />
-   //                   <button className="scroll-button" onClick={executeScroll}>
-   //                      {currentTheme ? <ScrollMouseLight /> : <ScrollMouse />}
-   //                   </button>
-   //                   <div style={{ marginBottom: "15vh" }} />
-   //                </div>
-   //             );
 
-   //          return (
-   //             <Fragment>
-   //                <div style={{ marginTop: "12vh" }} />
-   //                <ProjectCard key={index} data={project}></ProjectCard>
-   //                <div style={{ marginBottom: "15vh" }} />
-   //             </Fragment>
-   //          );
-   //       })}
-   //    </section>
-   // );
+   return (
+      <section className="projects-container">
+         {PROJECTS_DATA.map((project, index) => {
+            if (index === 0)
+               return (
+                  <div className="first-project">
+                     <div style={{ marginTop: "10vh" }} />
+                     <ProjectCard shakeAnimation={true} key={index} data={project} />
+                     <button className="scroll-button" onClick={executeScroll}>
+                        {currentTheme ? <ScrollMouseLight /> : <ScrollMouse />}
+                     </button>
+                     <div style={{ marginBottom: "15vh" }} />
+                  </div>
+               );
+
+            return (
+               <Fragment>
+                  <div style={{ marginTop: "12vh" }} />
+                  <ProjectCard key={index} data={project}></ProjectCard>
+                  <div style={{ marginBottom: "15vh" }} />
+               </Fragment>
+            );
+         })}
+      </section>
+   );
 };
 
 export default Projects;
